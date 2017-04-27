b92688f8442bab6b11ec42811b5a4fb0c8430a1e
valentincognito <valentin.vannay@gmail.com>
2017-04-27 17:15:34 +0900

    Add Menus

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangerArray;
+
+	private AudioSource audioSource;
+
+	void Awake(){
+		DontDestroyOnLoad (this);
+	}	
+	
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	void OnLevelWasLoaded(int level){
+		AudioClip thisLevelMusic = levelMusicChangerArray [level];
+
+		if(thisLevelMusic){
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.volume = 0.5f;
+			audioSource.Play();
+		}
+	}
+}
