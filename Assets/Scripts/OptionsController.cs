80dde79c78cecf628bf900ec96274c398bcca69c
valentincognito <valentin.vannay@gmail.com>
2017-04-28 18:25:17 +0900

    Add Volume Slider Option

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour {
+
+	public Slider volumeSlider;
+	public LevelManager levelManager;
+
+	private MusicManager musicManager;
+
+	// Use this for initialization
+	void Start () {
+		musicManager = GameObject.FindObjectOfType<MusicManager> ();
+
+		volumeSlider.value = PlayerPrefsManager.GetMasterVolume ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SaveAndExit(){
+		PlayerPrefsManager.SetMasterVolume (volumeSlider.value);
+		levelManager.LoadLevel ("01a_Start_Menu");
+	}
+}
