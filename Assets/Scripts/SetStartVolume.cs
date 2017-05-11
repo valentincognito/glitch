c3386626549bee0b4a9f2f4a728bb440ce878bec
valentincognito <valentin.vannay@gmail.com>
2017-05-11 17:11:22 +0900

    Finishing animation and prefabs

---
+++ Assets/Scripts/SetStartVolume.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStartVolume : MonoBehaviour {
+
+	private MusicManager musicManager;
+
+	// Use this for initialization
+	void Start () {
+		musicManager = GameObject.FindObjectOfType<MusicManager> ();
+		musicManager.SetVolume (PlayerPrefsManager.GetMasterVolume ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
