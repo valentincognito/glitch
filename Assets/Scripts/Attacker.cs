3de1e47a63cf8823e382596858ee4263c2c3a611
valentincognito <valentin.vannay@gmail.com>
2017-05-12 14:03:49 +0900

    Create attacker and defenders behaviour

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+
+
+	[Range(-1f, 1.5f)] private float currentSpeed;
+	private GameObject currentTarget;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D> ();
+		myRigidbody.isKinematic = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.left * currentSpeed * Time.deltaTime);
+	}
+		
+
+	public void setSpeed(float speed){
+		currentSpeed = speed;
+	}
+
+	// Call from the animator
+	public void StrikeCurrentTarget(float damage){
+		Debug.Log ("damage + " + damage);
+	}
+
+	public void Attack(GameObject obj){
+		currentTarget = obj;
+
+	}
+
+}
---
+++ Assets/Scripts/AttackersBehaviour/fox.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Attacker))]
+
+public class fox : MonoBehaviour {
+
+	private Animator anim;
+	private Attacker attacker;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		attacker = GetComponent<Attacker> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Debug.Log ("Fox collided with " + collider);
+
+		GameObject obj = collider.gameObject;
+
+		if(!obj.GetComponent<Defender>()){
+			return;
+		}
+
+		if (obj.GetComponent<Stone> ()) {
+			anim.SetTrigger ("jump trigger");
+		} else {
+			anim.SetBool ("isAttacking", true);
+			attacker.Attack (obj);
+		}
+
+	}
+
+}
