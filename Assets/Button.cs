ca484631a2608d4a8258802269add92fa8fac172
valentincognito <valentin.vannay@gmail.com>
2017-05-12 19:05:20 +0900

    Create Projectiles parent only if needed

---
+++ Assets/Button.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+	public GameObject defenderPrefab;
+
+	private Button[] buttonArray; 
+	public static GameObject selectedDefender;
+
+	// Use this for initialization
+	void Start () {
+		buttonArray = GameObject.FindObjectsOfType<Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown(){
+		foreach (Button thisButton in buttonArray) {
+			thisButton.GetComponent<SpriteRenderer> ().color = Color.black;
+		}
+
+		GetComponent<SpriteRenderer> ().color = Color.white;
+		selectedDefender = defenderPrefab;
+	}
+}
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour {
 
-	public GameObject projectile, projectileParent, gun;
+	public GameObject projectile, gun;
+
+	private GameObject projectileParent; 
+
+
+	void Start(){
+		projectileParent = GameObject.Find ("Projectiles");
+	
+		if(!projectileParent){
+			projectileParent = new GameObject("Projectiles");
+		}
+	}
 
 	private void Fire(){
+
 		GameObject newProjectile = Instantiate (projectile) as GameObject;
 		newProjectile.transform.parent = projectileParent.transform;
 		newProjectile.transform.position = gun.transform.position;
