7b60d11c71306b051d217531ead0fcd49f05d5f9
valentincognito <valentin.vannay@gmail.com>
2017-05-15 18:06:47 +0900

    Fix gravestone behaviour

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,23 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Button : MonoBehaviour {
 
 	public GameObject defenderPrefab;
+	public static GameObject selectedDefender;
 
 	private Button[] buttonArray; 
-	public static GameObject selectedDefender;
+	private Text costText;
 
 	// Use this for initialization
 	void Start () {
 		buttonArray = GameObject.FindObjectsOfType<Button> ();
+
+		costText = GetComponentInChildren<Text> ();
+		costText.text = defenderPrefab.GetComponent<Defender> ().starCost.ToString ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 
 	void OnMouseDown(){
 		foreach (Button thisButton in buttonArray) {
