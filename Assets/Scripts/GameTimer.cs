17eb659eb490d16fd5967e9102c13c1dbc784740
valentincognito <valentin.vannay@gmail.com>
2017-05-15 17:33:49 +0900

    Handling win condition

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour {
+
+	public float levelSeconds = 30f;
+	public AudioSource audioSource;
+	public LevelManager levelManager;
+
+	private Slider slider;
+	private GameObject winLabel;
+	private bool isEndOfLevel = false;
+
+	// Use this for initialization
+	void Start () {
+		slider = GetComponent<Slider> ();
+		audioSource = GetComponent<AudioSource> ();
+		winLabel = GameObject.Find("Win Text");
+		winLabel.SetActive (false);
+
+		slider.value = Time.timeSinceLevelLoad / levelSeconds;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		slider.value = Time.timeSinceLevelLoad / levelSeconds;
+
+		if(Time.timeSinceLevelLoad >= levelSeconds && !isEndOfLevel){
+			audioSource.Play ();
+			winLabel.SetActive (true);
+			Invoke("LoadVictoryScreen", audioSource.clip.length);
+			isEndOfLevel = true;
+		}
+
+	}
+		
+
+	private void LoadVictoryScreen(){
+		levelManager.LoadLevel ("03a_Win_Screen");
+	}
+}
